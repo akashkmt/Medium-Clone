4a87b217306ac00a6fe5d05646c9a4b6c348c621
Akash Kumawat <akashkmt963@gmail.com>
2022-07-27 14:27:06 +0530

    initial Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ChakraProvider} from '@chakra-ui/react';
+
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1>Hello</h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <App />
+);
+
