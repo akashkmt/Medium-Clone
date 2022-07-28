dc74dd30087032180521dd9025151f79835c5f2d
Akash Kumawat <akashkmt963@gmail.com>
2022-07-28 18:44:54 +0530

    some routes add

--- Server/handlers/user.js
+++ Server/handlers/user.js
@@ -53,10 +53,75 @@ const isLoggedIn = async(req, res) => {
     }
 }
 
+//follow a user
+const followUser = async(req, res) => {
+    try {
+        const {userId} = req.params;   //user to follow
+        const {token} = req.headers;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded._id);  //user who is following
+        const following = user.following;  //array of users who is following
+        if (following.includes(userId)) {
+            return res.status(400).send({message: 'Already following'});  //if user is already following
+        }
+        following.push(userId);  //add user to following array
+        await User.findByIdAndUpdate(decoded._id, {following});  //update user
 
+        const follower = await User.findById(userId);  //user who is being followed
+        const followers = follower.followers;  //array of followers
+        if (!followers.includes(decoded._id)) {
+            followers.push(decoded._id);  //add user to followers array
+            await User.findByIdAndUpdate(userId, {followers});  //update followers array
+        }
+
+        return res.status(200).send({message: 'Followed'});
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
+
+//unfollow a user
+const unfollowUser = async(req, res) => {
+    try {
+        const {userId} = req.params;   //user to unfollow
+        const {token} = req.headers;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded._id);  //user who is unfollowing
+        const following = user.following;  //array of users who is following
+        if (!following.includes(userId)) {
+            return res.status(400).send({message: 'Not following'});  //if user is not following
+        }
+        following.splice(following.indexOf(userId), 1);  //remove user from following array
+        await User.findByIdAndUpdate(decoded._id, {following});  //update user
+
+        const follower = await User.findById(userId);  //user who is being unfollowed
+        const followers = follower.followers;  //array of followers
+        followers.splice(followers.indexOf(decoded._id), 1);  //remove user from followers array
+        await User.findByIdAndUpdate(userId, {followers});  //update followers array
+
+        return res.status(200).send({message: 'Unfollowed'});
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
+
+//get a user
+const getUser = async(req, res) => {
+    try {
+        const {token} = req.headers;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded._id);
+        return res.status(200).send(user);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+}
 
 module.exports = {
     createUser,
     loginUser,
-    isLoggedIn
+    isLoggedIn,
+    followUser,
+    unfollowUser,
+    getUser
 }
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createUser, loginUser, isLoggedIn } = require('../handlers/user');
+const { createUser, loginUser, isLoggedIn, followUser, unfollowUser, getUser } = require('../handlers/user');
 
 const userRouter = express.Router();
 
 userRouter.post('/createUser', createUser);
 userRouter.post('/loginUser', loginUser);
 userRouter.post('/isLoggedIn', isLoggedIn);
-
+userRouter.post('/followUser/:userId', followUser);
+userRouter.post('/unfollowUser/:userId', unfollowUser);
+userRouter.get('/getUser', getUser);
 
 module.exports = {userRouter};
